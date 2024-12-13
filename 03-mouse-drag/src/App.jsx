162d472bb28e251caebd71b2cc150724927fff44
Joseito <joseprados1998@gmail.com>
2024-12-13 14:16:14 +0100

    updated thrid project

--- 03-mouse-drag/src/App.jsx
+++ 03-mouse-drag/src/App.jsx
@@ -1,8 +1,57 @@
 import './App.css'
+import { useEffect, useState } from 'react'
+
+const FollowMouse = () => {
+  const [enabled, changeState] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  const changeButton = () => {
+    const newState = enabled ? false : true
+    changeState(newState)
+  }
+  useEffect(() => {
+    console.log("efecto")
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      console.log('handlemove: ', { clientX, clientY })
+      setPosition({ x: clientX, y: clientY })
+    }
+    if (enabled) {
+      window.addEventListener("pointermove", handleMove)
+    }
+
+    //limpieza cuando el componente se desmonta
+    return () => {
+      console.log("cleanUp")
+      window.removeEventListener("pointermove", handleMove)
+    }
+  }, [enabled])
+  return(
+    <>
+    <div style={{
+      position: 'absolute',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      border: '1px solid #fff',
+      borderRadius: '50%',
+      opacity: 0.8,
+      pointerEvents: 'none',
+      left: -25,
+      top: -25,
+      width: 50,
+      height: 50,
+      transform: `translate(${position.x}px, ${position.y}px)`
+    }}/>
+  
+    <button onClick={changeButton}> {enabled ? "Desactivar" : "Activar"}</button>
+  </>)
+  }
 
 function App() {
   return (
-    <h3>Proyecto 3</h3>
+    <main>
+      <FollowMouse />
+    </main>
   )
 }
 
