e6731f4bcf52461f5ac5a8866cfc7b39777db32b
Joseito <joseprados1998@gmail.com>
2024-12-17 09:30:06 +0100

    3hecho

--- 03-mouse-drag/src/App.jsx
+++ 03-mouse-drag/src/App.jsx
@@ -27,25 +27,34 @@ const FollowMouse = () => {
       window.removeEventListener("pointermove", handleMove)
     }
   }, [enabled])
-  return(
+
+  useEffect(() => {
+    document.body.classList.toggle("no-cursor", enabled)
+
+    return () => {
+      document.body.classList.remove("no-cursor", !enabled)
+    }
+  }, [enabled])
+
+  return (
     <>
-    <div style={{
-      position: 'absolute',
-      backgroundColor: 'rgba(0, 0, 0, 0.5)',
-      border: '1px solid #fff',
-      borderRadius: '50%',
-      opacity: 0.8,
-      pointerEvents: 'none',
-      left: -25,
-      top: -25,
-      width: 50,
-      height: 50,
-      transform: `translate(${position.x}px, ${position.y}px)`
-    }}/>
-  
-    <button onClick={changeButton}> {enabled ? "Desactivar" : "Activar"}</button>
-  </>)
-  }
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }} />
+
+      <button onClick={changeButton}> {enabled ? "Desactivar" : "Activar"}</button>
+    </>)
+}
 
 function App() {
   return (
